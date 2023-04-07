b26efa35e54937dc245a0868083f3696a453a6f7
Raymond <raymond_harris@ssg.gov.sg>
2023-04-07 23:41:21 +0800

    Fixed target drift

--- src/monitor.py
+++ src/monitor.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 from evidently.report import Report
-from evidently.metric_preset import DataDriftPreset, TargetDriftPreset, DataQualityPreset
+from evidently.metric_preset import DataDriftPreset, TargetDriftPreset, DataQualityPreset, RegressionPreset
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -9,7 +9,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.dirname(script_dir)
 
 def monitor_data_drift(reference_df, current_df):
-    drift_report = Report(metrics=[DataDriftPreset(), TargetDriftPreset()])
+    drift_report = Report(metrics=[DataDriftPreset()])
     drift_report.run(reference_data=reference_df, current_data=current_df)
 
     if not os.path.exists(os.path.join(root_dir, 'monitor')):
@@ -17,7 +17,17 @@ def monitor_data_drift(reference_df, current_df):
     drift_report.save_html(os.path.join(root_dir, 'monitor/data_drift.html'))
 
 def monitor_target_drift(reference_df, current_df):
-    pass
+    target_drift_report = Report(metrics=[TargetDriftPreset()])
+    target_drift_report.run(reference_data=reference_df, current_data=current_df)
+    target_drift_report.save_html(os.path.join(root_dir, 'monitor/target_drift.html'))
+
+# def monitor_regression_performance(reference_df, current_df):
+#     regression_performance_report = Report(metrics=[
+#         RegressionPreset(),
+#     ])
+
+#     regression_performance_report.run(reference_data=reference_df, current_data=current_df)
+#     regression_performance_report.save_html(os.path.join(root_dir, 'monitor/regression_performance.html'))
 
 if __name__ == "__main__":
     
@@ -28,10 +38,18 @@ if __name__ == "__main__":
 
     #Split df to the inputs and target cols
     keep_columns = [col for col in reference_df.columns if not col in target_cols]
+    target_reference_df = reference_df[target_cols]
     reference_df = reference_df[keep_columns]
+    target_change_df = change_df[target_cols]
     change_df = change_df[keep_columns]
 
     if change_df.empty:
         print("No change in data, skipping monitoring...")
     else:
         monitor_data_drift(reference_df, change_df)
+        #Target drift and regression report requires 'target' cols
+        reference_df['target'] = target_reference_df['psm']
+        change_df['target'] = target_change_df['psm']
+
+        monitor_target_drift(reference_df, change_df)
+        # monitor_regression_performance(reference_df, change_df)
